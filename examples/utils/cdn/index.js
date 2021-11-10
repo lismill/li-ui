dbde4263a90e53eda76f8c1f751efa2b96bebcd3
Lean <lean.li@71360.com>
2021-11-10 18:22:47 +0800

    feat: init

--- examples/utils/cdn/index.js
+++ examples/utils/cdn/index.js
@@ -6,25 +6,4 @@ module.exports.CDN = {
       "https://cdn.jsdelivr.net/npm/vue-i18n@8.26.5/dist/vue-i18n.js",
     ],
   },
-  test: {
-    js: [
-      "https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js",
-      "https://cdn.jsdelivr.net/npm/vue-router@3.2.0/dist/vue-router.js",
-      "https://cdn.jsdelivr.net/npm/vue-i18n@8.26.5/dist/vue-i18n.js",
-    ],
-  },
-  uat: {
-    js: [
-      "https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.min.js",
-      "https://cdn.jsdelivr.net/npm/vue-router@3.2.0/dist/vue-router.min.js",
-      "https://cdn.jsdelivr.net/npm/vue-i18n@8.26.5/dist/vue-i18n.min.js",
-    ],
-  },
-  production: {
-    js: [
-      "https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.min.js",
-      "https://cdn.jsdelivr.net/npm/vue-router@3.2.0/dist/vue-router.min.js",
-      "https://cdn.jsdelivr.net/npm/vue-i18n@8.26.5/dist/vue-i18n.min.js",
-    ],
-  },
 };
