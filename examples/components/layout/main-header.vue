f3b534cf17c2926947d0723ae32b39cd2348d6c3
Lean <lean.li@71360.com>
2021-11-11 16:24:35 +0800

    feat: init

--- examples/components/layout/main-header.vue
+++ examples/components/layout/main-header.vue
@@ -2,20 +2,15 @@
   <header class="app-header">
     <div class="header-container">
       <div class="logo">
-        <svg-icon v-if="random === 0" name="logo-666" size="66"></svg-icon>
-        <svg-icon v-if="random === 1" name="logo-rock" size="66"></svg-icon>
-        <svg-icon v-if="random === 2" name="logo-爱心" size="66"></svg-icon>
-        <svg-icon v-if="random === 3" name="logo-爱心气泡" size="66"></svg-icon>
-        <svg-icon v-if="random === 4" name="logo-冰淇淋" size="66"></svg-icon>
-        <svg-icon v-if="random === 5" name="logo-吹风机" size="66"></svg-icon>
-        <svg-icon v-if="random === 6" name="logo-红包" size="66"></svg-icon>
-        <svg-icon v-if="random === 7" name="logo-金元宝" size="66"></svg-icon>
-        <svg-icon v-if="random === 8" name="logo-开心" size="66"></svg-icon>
-        <svg-icon v-if="random === 9" name="logo-奶茶" size="66"></svg-icon>
-        <svg-icon v-if="random === 10" name="logo-游戏机" size="66"></svg-icon>
-        <svg-icon v-if="random === 11" name="logo-游戏机手柄" size="66"></svg-icon>
-        <svg-icon v-if="random === 12" name="logo-面条" size="58" style="margin: 0 5px 0 -5px"></svg-icon>
-        <svg-icon v-if="random === 13" name="logo-小女孩" size="45" style="margin: 0 10px 0 0"></svg-icon>
+        <template v-for="(logo, index) in logos">
+          <svg-icon
+            :key="logo.name"
+            v-if="random === index"
+            :name="logo.name"
+            :size="logo.size"
+            :style="{ marginLeft: logo.left, marginRight: logo.right }"
+          ></svg-icon>
+        </template>
         <span>Generate a library for vue.js2</span>
       </div>
       <div class="link">
@@ -40,11 +35,33 @@ import markdown from "@/router/markdown";
   components: {},
 })
 export default class AppHeader extends Vue {
+  logos = [
+    { name: "logo-666", size: "66" },
+    { name: "logo-rock", size: "66" },
+    { name: "logo-爱心", size: "66" },
+    { name: "logo-爱心气泡", size: "66" },
+    { name: "logo-红包", size: "66" },
+    { name: "logo-开心", size: "66" },
+    { name: "logo-游戏机", size: "66" },
+    { name: "logo-春", size: "50", left: "0px", right: "10px" },
+    { name: "logo-火箭", size: "50", left: "0px", right: "10px" },
+    { name: "logo-缆车", size: "50", left: "0px", right: "10px" },
+    { name: "logo-气球", size: "50", left: "0px", right: "10px" },
+    { name: "logo-北京", size: "55", left: "-4px" },
+    { name: "logo-上海", size: "55", left: "-4px" },
+    { name: "logo-广州", size: "55", left: "-4px" },
+    { name: "logo-深圳", size: "55", left: "-4px" },
+    { name: "logo-购物车", size: "50", left: "0", right: "10px" },
+    { name: "logo-橘猫", size: "55", left: "0px", right: "10px" },
+    { name: "logo-可爱的妹妹", size: "60", left: "-2px", right: "10px" },
+    { name: "logo-青色小怪兽", size: "55", left: "-2px", right: "10px" },
+    { name: "logo-阳小妹", size: "45", left: "0", right: "10px" },
+  ];
   markdown = markdown;
   // 随机logo
   random = 0;
   created() {
-    this.random = Math.floor(Math.random() * 14);
+    this.random = Math.floor(Math.random() * this.logos.length);
   }
 
   // 当前路由
--- examples/views/404/index.vue
+++ examples/views/404/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="404">404</div>
+  <div class="markdown-body 404">404</div>
 </template>
 
 <script lang="ts">
